a6b1cc903a196047a23a32843410d2ead1aa60b1
AlessandroVe <aveneziano536@gmail.com>
2021-06-16 10:34:54 +0200

    .

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -7,3 +7,8 @@ var palla = {
     peso : 10
 }
 console.log(palla);
+/* Snack1. - la continua.
+Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla. */
+palla.peso =parseInt(prompt("modificare il peso della palla"));
+
+console.log(palla);
