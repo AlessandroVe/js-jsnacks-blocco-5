e62aace603e75ab1bc41f1fb70e44c4771bfef35
AlessandroVe <aveneziano536@gmail.com>
2021-06-16 11:55:27 +0200

    .

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -1,3 +1,15 @@
+/* Snack 2
+Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+Calcolare perimetro e area e stampare il risultato con console.log */
 document.addEventListener("DOMContentLoaded", function(){
+    var triangolorettangolo={
+        base : 15,
+        altezza : 20,
+    }
+    console.log("l'area del triangolo e' di " + (triangolorettangolo.base*triangolorettangolo.altezza)/2);
+
+    var i= Math.sqrt( Math.pow(triangolorettangolo.base,2) + (Math.pow(triangolorettangolo.height,2) ));
+    
+    console.log("il perimetro del triangolo e' di "+ i + triangolorettangolo.base + triangolorettangolo.height )
 
 })
